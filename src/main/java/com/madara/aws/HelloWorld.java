de7522d88ee72209dd7890c8559c659c955248fd
hsmdayananda <you@example.com>
2018-07-30 03:03:30 +0530

    add dependency

--- src/main/java/com/madara/aws/HelloWorld.java
+++ src/main/java/com/madara/aws/HelloWorld.java
@@ -4,7 +4,11 @@ package com.madara.aws;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.ComponentScan;
+
+
 @SpringBootApplication
+@ComponentScan(basePackages = "com.madara.aws")
 public class HelloWorld {
 
     public static void main(String[] args) {
--- src/main/java/com/madara/aws/controller/HelloWorldController.java
+++ src/main/java/com/madara/aws/controller/HelloWorldController.java
@@ -1,15 +1,18 @@
 package com.madara.aws.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/madara")
+
 @RestController
 public class HelloWorldController {
 
-    @RequestMapping(method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
     public String test() {
         return "Hello H Madara";
     }
